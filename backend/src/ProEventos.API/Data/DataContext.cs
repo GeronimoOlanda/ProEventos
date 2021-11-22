1f98dfaac3d2c651304e8039425233a98c39bf4b
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-11-21 23:49:44 -0300

    Criando nosso data context e settando  a conexao no json

--- backend/src/ProEventos.API/Data/DataContext.cs
+++ backend/src/ProEventos.API/Data/DataContext.cs
@@ -5,6 +5,12 @@ namespace ProEventos.API.Data
 {
     public class DataContext : DbContext
     {
+    
+        // Base() passa para o elemento pai
+        // constructor
+        public DataContext(DbContextOptions<DataContext> options): base(options){
+
+        }
         public DbSet<Evento> Eventos { get; set; }
     }
 }
