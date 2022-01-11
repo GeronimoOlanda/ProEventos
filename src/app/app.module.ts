b4043ab27304fdea5dc07ed35f9c5477b30a4e3d
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-01-11 11:14:27 -0300

    u

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'; //http cliente para acessar a api esse module
+import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { NavComponent } from './nav/nav.component';
-
 import {CollapseModule} from 'ngx-bootstrap/collapse';
+
 
 @NgModule({
   declarations: [
@@ -21,6 +23,7 @@ import {CollapseModule} from 'ngx-bootstrap/collapse';
     HttpClientModule, //importante o http client
     BrowserAnimationsModule,
     CollapseModule.forRoot(),
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/eventos/eventos.component.ts
+++ src/app/eventos/eventos.component.ts
@@ -7,7 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.css']
 })
 export class EventosComponent implements OnInit {
+
   public eventos: any = [];
+  public filteredEvents: any = []; //inicializando a variavel para o filtro filtrado
+  widthImg: number = 50;
+  marginImg: number = 2;
+  showImg: boolean = true;
+  borderImg: number = 5;
+
+  private _listFilter: string = '';
+  //retorna o que foi filtrado
+  public get listFilter(){
+    return this._listFilter;
+  }
+  //pega os valores para serem filtrados
+  public set listFilter(value: string){
+    this._listFilter = value;
+    this.filteredEvents = this.listFilter ? this.filterEvents(this.listFilter) : this.eventos // ternario para verificar os valores a serem filtrados
+  }
+  //fazendo o filtro para que tudo seja filtrado em letra minuscula e que seja diferente de vazio
+  filterEvents(FilterBy: string): any {
+    FilterBy = FilterBy.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(FilterBy) !== -1
+    );
+
+  }
+
 
   constructor(private http: HttpClient) {
 
@@ -16,11 +42,16 @@ export class EventosComponent implements OnInit {
   ngOnInit(): void {
     this.getEventos()
   }
-
+  ModifyStateImg(){
+    this.showImg = !this.showImg;
+  }
   public getEventos() : void {
 
     this.http.get('https://localhost:44367/api/Evento/').subscribe(
-      response => this.eventos = response,
+      response =>{
+         this.eventos = response
+        this.filteredEvents = this.eventos
+      },
       error => console.log(error),
     );
 
